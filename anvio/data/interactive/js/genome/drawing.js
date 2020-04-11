0007b6b350585a4b810ee63fba325c1e7169674a
Ozcan Esen <ozcanesen@gmail.com>
2020-04-11 08:01:13 -0500

    fix drawing

--- anvio/data/interactive/js/genome/drawing.js
+++ anvio/data/interactive/js/genome/drawing.js
@@ -1,69 +1,75 @@
 class Layer {
-    constructor(width, height) {
-        this.width = width
-        this.height = height
+  constructor(width, height) {
+    this.width = width
+    this.height = height
 
-        this.objects = []
-        this.knownShapes = ['rectangle', 'line', 'path']
+    this.objects = []
+    this.knownShapes = ['rectangle', 'line', 'path']
 
-        this.knownShapes.forEach((currentShape) => {
-            // This creates member functions like Layer.rectangle()
-            this[currentShape] = (params) => {
-                this.objects.push({
-                    'shape': currentShape,
-                    'params': params
-                })
-            }
+    this.knownShapes.forEach((currentShape) => {
+      // This creates member functions like Layer.rectangle()
+      this[currentShape] = (params) => {
+        this.objects.push({
+          'shape': currentShape,
+          'params': params
         })
-    }
+      }
+    })
+  }
 }
 
 
 class RenderCanvas {
-    constructor(layer, xScale = 1, yScale = 1) {
-        this.layer = layer
-        this.xScale = xScale
-        this.yScale = yScale
+  constructor(layer, xScale, yScale) {
+    this.layer = layer
+    this.xScale = xScale
+    this.yScale = yScale
+  }
+
+  getBuffer() {
+    const xS = (val) => {
+      return val * this.xScale
+    }
+    const yS = (val) => {
+      return val * this.yScale
     }
 
-    getBuffer() {
-        const xS = (val) => val * this.xScale
-        const yS = (val) => val * this.yScale
+    const buffer = new OffscreenCanvas(xS(this.layer.width), yS(this.layer.height))
+    const ctx = buffer.getContext('2d')
 
-        const buffer = new OffscreenCanvas(this.layer.width, this.layer.height)
-        const ctx = buffer.getContext('2d')
+    for (const obj of this.layer.objects) {
+      let shape = obj.shape
+      let params = obj.params
 
-        for (const obj of this.layer.objects) {
-            let shape = obj.shape
-            let params = obj.params
+      ctx.beginPath()
 
-            ctx.beginPath()
+      if (shape == 'rectangle') {
+        ctx.rect(
+          xS(params.x),
+          yS(params.y),
+          xS(params.width),
+          yS(params.height)
+        )
+      }
 
-            if (shape == 'rectangle') {
-                ctx.rect(xS(params.x),
-                    yS(params.y),
-                    xS(params.width),
-                    yS(params.height))
-            }
+      if (params.hasOwnProperty('fillStyle')) {
+        ctx.fillStyle = params.fillStyle
+      }
 
-            if (params.hasOwnProperty('fillStyle')) {
-                ctx.fillStyle = params.fillStyle
-            }
-
-            if (params.hasOwnProperty('fill') && params.fill) {
-                ctx.fill()
-            } else {
-                ctx.stroke()
-            }
-        }
-
-        return buffer
+      if (params.hasOwnProperty('fill') && params.fill) {
+        ctx.fill()
+      } else {
+        ctx.stroke()
+      }
     }
+
+    return buffer
+  }
 }
 
 
 
 export {
-    Layer,
-    RenderCanvas,
+  Layer,
+  RenderCanvas,
 }
--- anvio/data/interactive/js/genome/genomeTrack.js
+++ anvio/data/interactive/js/genome/genomeTrack.js
@@ -1,39 +1,47 @@
-import { Contig } from './contig.js';
+import {
+  Contig
+} from './contig.js';
 
 class GenomeTrack {
-    constructor(viewer, name) {
-        this.viewer = viewer;
-        this.name = name;
-        this.contigs = [];
+  constructor(viewer, name) {
+    this.viewer = viewer;
+    this.name = name;
+    this.contigs = [];
+  }
+
+  getContig(contigName) {
+    let result = this.contigs.find((contig) => contig.name == contigName);
+
+    if (typeof result === 'undefined') {
+      throw `Contig "${contigName}" not found in the genome track "${this.name}".`;
     }
 
-    getContig(contigName) {
-        let result = this.contigs.find((contig) => contig.name == contigName);
-        
-        if (typeof result === 'undefined') {
-            throw `Contig "${contigName}" not found in the genome track "${this.name}".`;
-        }
+    return result;
+  }
 
-        return result;
-    }
+  getLongestContig() {
+    return Math.max(...this.contigs.map((contig) => contig.length));
+  }
 
-    addContig(contigData) {
-        let contig = new Contig(this.viewer);
-        
-        contig.name = contigData.name;
-        contig.length = contigData.length;
+  addContig(contigData) {
+    let contig = new Contig(this.viewer);
 
-        this.contigs.push(contig);
-    }
+    contig.name = contigData.name;
+    contig.length = contigData.length;
 
-    getLayers() {
-        let layers
-        this.contigs.forEach((contig) => {
-            layers.push(contig.getLayer())
-        });
-        return layers
-    }
+    this.contigs.push(contig);
+  }
+
+  getLayers() {
+    let layers = [];
+    this.contigs.forEach((contig) => {
+      layers.push(contig.getLayers())
+    });
+    return layers
+  }
 }
 
 
-export { GenomeTrack };
+export {
+  GenomeTrack
+};
